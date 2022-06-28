7f1c4e7ca2be255be7f16df1de37834c000556af
gauravposwal <contact.mr.poswal@gmail.com>
2022-06-28 10:17:12 +0530

    - Create Custom Tabs & Modify logic for viewPager scroll

--- app/src/main/java/com/fankonnect/app/login/ui/LaunchCarouselActivity.kt
+++ app/src/main/java/com/fankonnect/app/login/ui/LaunchCarouselActivity.kt
@@ -3,22 +3,24 @@ package com.fankonnect.app.login.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
 import com.fankonnect.app.R
 import com.fankonnect.app.login.adapter.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_launch_carousel.*
-import java.util.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+import android.view.ViewGroup
+import android.widget.LinearLayout
+
 
 class LaunchCarouselActivity : AppCompatActivity() {
 
-    private var currentPage = 0
-
     companion object {
-        private const val NUM_PAGES = 4
-        private const val DELAY_MS = 500L
-        private const val PERIOD_MS = 3000L
         private const val TAG = "LaunchCarouselActivity"
         fun startActivity(context: Context) {
             Intent(context, LaunchCarouselActivity::class.java).apply {}
@@ -31,29 +33,20 @@ class LaunchCarouselActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_carousel)
-
         setUpPager()
-        runInfiniteCarousel()
-    }
-
-    private fun runInfiniteCarousel() {
-
-        val handler = Handler()
-        val update = Runnable {
-            if (currentPage == NUM_PAGES - 1) {
-                currentPage = 0
+        MainScope().launch {
+            getViewPagerPages().collect {
+                carouselViewPager.currentItem = it
             }
-            carouselViewPager.setCurrentItem(currentPage++, true)
         }
-
-       val timer = Timer() // This will create a new Thread
-        timer.schedule(object : TimerTask() {
-            // task to be scheduled
-           override fun run() {
-                handler.post(update)
-            }
-        }, DELAY_MS, PERIOD_MS)
     }
+
+    private fun getViewPagerPages() = flow {
+        repeat(3) {
+            delay(2000)
+            emit(it)
+        }
+    }.flowOn(Dispatchers.Default)
 
     private fun setUpPager() {
         ViewPagerAdapter(supportFragmentManager).apply {
@@ -63,19 +56,12 @@ class LaunchCarouselActivity : AppCompatActivity() {
 
         }.also {
             carouselViewPager.adapter = it
-            carouselViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-                override fun onPageScrolled(
-                    position: Int,
-                    positionOffset: Float,
-                    positionOffsetPixels: Int
-                ) {}
-
-                override fun onPageSelected(position: Int) {
-                }
-
-                override fun onPageScrollStateChanged(state: Int) {}
-            })
         }
         tabLayout.setupWithViewPager(carouselViewPager)
+        for (i in 0 until tabLayout.tabCount) {
+            val tab: View = (tabLayout.getChildAt(0) as ViewGroup).getChildAt(i)
+            (tab.layoutParams as LinearLayout.LayoutParams).setMargins(8, 0, 8, 0)
+            tab.requestLayout()
+        }
     }
 }
