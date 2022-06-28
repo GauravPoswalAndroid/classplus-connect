2c17088a8a98c2843ba6483239a633076957649f
gauravposwal <contact.mr.poswal@gmail.com>
2022-06-28 11:26:54 +0530

    - Add Carousel Data/Assets statically

--- app/src/main/java/com/fankonnect/app/login/ui/LaunchCarouselActivity.kt
+++ app/src/main/java/com/fankonnect/app/login/ui/LaunchCarouselActivity.kt
@@ -4,7 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.viewpager.widget.ViewPager
 import com.fankonnect.app.R
 import com.fankonnect.app.login.adapter.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_launch_carousel.*
@@ -14,8 +19,6 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.launch
-import android.view.ViewGroup
-import android.widget.LinearLayout
 
 
 class LaunchCarouselActivity : AppCompatActivity() {
@@ -33,11 +36,18 @@ class LaunchCarouselActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_carousel)
+        setUpViews()
         setUpPager()
         MainScope().launch {
             getViewPagerPages().collect {
                 carouselViewPager.currentItem = it
             }
+        }
+    }
+
+    private fun setUpViews() {
+        tvLoginSignUp.setOnClickListener{
+            LoginActivity.startActivity(this)
         }
     }
 
@@ -48,14 +58,33 @@ class LaunchCarouselActivity : AppCompatActivity() {
         }
     }.flowOn(Dispatchers.Default)
 
+    private fun highlightCTA() {
+        tvLoginSignUp.background =
+            ResourcesCompat.getDrawable(resources, R.drawable.bg_button_click_color_primary, theme)
+        tvLoginSignUp.setTextColor(ContextCompat.getColor(this, R.color.white))
+    }
+
     private fun setUpPager() {
         ViewPagerAdapter(supportFragmentManager).apply {
-            addFragment(LaunchCarouselFragment.newInstance(), "")
-            addFragment(LaunchCarouselFragment.newInstance(), "")
-            addFragment(LaunchCarouselFragment.newInstance(), "")
+            addFragment(LaunchCarouselFragment.newInstance(R.drawable.ic_carousel_image1, getString(R.string.carousel_text1)), "")
+            addFragment(LaunchCarouselFragment.newInstance(R.drawable.ic_carousel_image2, getString(R.string.carousel_text2)), "")
+            addFragment(LaunchCarouselFragment.newInstance(R.drawable.ic_carousel_image3, getString(R.string.carousel_text3)), "")
 
         }.also {
             carouselViewPager.adapter = it
+            carouselViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
+                    if(position == 2) highlightCTA()
+                }
+
+                override fun onPageSelected(position: Int) {}
+
+                override fun onPageScrollStateChanged(state: Int) {}
+            })
         }
         tabLayout.setupWithViewPager(carouselViewPager)
         for (i in 0 until tabLayout.tabCount) {
--- app/src/main/java/com/fankonnect/app/login/ui/LaunchCarouselFragment.kt
+++ app/src/main/java/com/fankonnect/app/login/ui/LaunchCarouselFragment.kt
@@ -1,18 +1,22 @@
 package com.fankonnect.app.login.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.fankonnect.app.R
+import kotlinx.android.synthetic.main.fragment_launch_carousel.*
 
 class LaunchCarouselFragment : Fragment() {
+    private var carouselImage: Int? = null
+    private var carouselText: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
+            carouselImage = it.getInt(CAROUSEL_IMAGE_PATH)
+            carouselText = it.getString(CAROUSEL_TEXT)
         }
     }
 
@@ -23,12 +27,30 @@ class LaunchCarouselFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_launch_carousel, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setCarouselData()
+    }
+
+    private fun setCarouselData() {
+        carouselImage?.let {
+            ivCarouselImage.setImageResource(it)
+        }
+        carouselText?.let {
+            tvCarouselText.text = it
+        }
+    }
+
     companion object {
+        private const val CAROUSEL_IMAGE_PATH = "CAROUSEL_IMAGE_PATH"
+        private const val CAROUSEL_TEXT = "CAROUSEL_TEXT"
+
         @JvmStatic
-        fun newInstance() =
+        fun newInstance(imageSrc: Int, text: String) =
             LaunchCarouselFragment().apply {
                 arguments = Bundle().apply {
-
+                    putInt(CAROUSEL_IMAGE_PATH, imageSrc)
+                    putString(CAROUSEL_TEXT, text)
                 }
             }
     }
